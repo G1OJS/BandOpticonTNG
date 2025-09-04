4a8a6205c6d3903653fa37683cb18646f9322d96
G1OJS <140509227+G1OJS@users.noreply.github.com>
2025-09-04 09:57:15 +0100

    fix grid layout

--- src/main.js
+++ src/main.js
@@ -4,81 +4,71 @@ import {loadConfig, myCall} from './store-cfg.js';
 import {mhToLatLong} from './geo.js';
 import Ribbon from './ribbon.js';
 
+
+const colors = {myCall_tx:'rgba(255, 20, 20, 1)', leader_tx:'rgba(255, 99, 132, .5)', all_tx:'rgba(255, 99, 132, 0.3)',
+				myCall_rx:'rgba(20, 20, 200, 1)', leader_rx:'rgba(54, 162, 200, 0.5)',all_rx:'rgba(54, 162, 200, 0.3)'};
+
 const ribbon = new Ribbon({
   onModeChange: refreshAll,
   onConfigChange: refreshAll,
-});
+  onBandsChange: refreshAll
+ });
 
 let getMode = () => null;
+let getBands = () => null;
 let mode = null;
-let band = null;
-let homeCalls = null;
-let leaderCall = null;
-let	ocs_myCall1 = null;
-let	ocs_Leader = null;
-let	ocs_All = null;
+let bands = null;
 
-let connsData = null;
 let myCall1 = null;
 let myCall2 = null;
-let RxTx = 0;
-let container = null;
-let activeBands = ['160m','80m','40m','30m','20m','15m','10m','2m','70cm'];
-let band_idx=0;
 let charts={};
 
 setInterval(() => purgeLiveConnections(), 5000);
 setInterval(() => ribbon.writeModeButtons(), 5000);
-setInterval(() => refreshBand(), 50);
+setInterval(() => refreshBands(), 1000);
 setInterval(() => refreshAll(), 5000);
 
-
 loadConfig();
+myCall1 = myCall.split(",")[0].trim();
+write_mainView();
 connectToFeed();
-writeGrid();
 
-// event listeners for buttons 
-document.addEventListener('click', (e) => {
-	const action = e.target.dataset.action;
-	if (!action) return;
-	loadView(action,  e.target.dataset.band);
-//	console.log("ui-core: clicked with action "+e.target.dataset.action+" and band "+e.target.dataset.band);
-});
+function write_mainView(){
+	let html = "<h3>Tx and Rx performance for all callsigns in home squares with band leader and "+myCall1+" overlaid</h3>";
 
-function wavelength(band) {
-    let wl = parseInt(band.split("m")[0]);
-    if (band.search("cm") > 0) {
-        return wl / 100
-    } else {
-        return wl
-    }
+	html +="<div id='canvasGrid'>";
+	html +="<div class='gridHeader'><table><tr><td colspan='3'>Receive</td><td colspan='3'>Transmit</td></tr><tr>";
+	html +="<td><span class = 'legendMarker' style='background:" +  colors.myCall_rx + "'></span>"+myCall1+"</td>";
+	html +="<td><span class = 'legendMarker' style='background:" +  colors.leader_rx + "'></span>Band leader</td>";
+	html +="<td><span class = 'legendMarker' style='background:" +  colors.all_rx + "'></span>All home  </td>";
+	html +="<td><span class = 'legendMarker' style='background:" +  colors.myCall_tx + "'></span>"+myCall1+"</td>";
+	html +="<td><span class = 'legendMarker' style='background:" +  colors.leader_tx + "'></span>Band leader</td>";
+	html +="<td><span class = 'legendMarker' style='background:" +  colors.all_tx + "'></span>All home</td>";
+	html +="</tr></table></div>";
+ 	for (let i =0;i<15;i++){
+		html += "<div class = 'canvasHolder hidden' id = 'div_"+i+"'><canvas id='canvas_"+i+"'></canvas></div>";	
+	}	
+	html +="</div>";
+	
+	document.getElementById("mainView").innerHTML = html;
 }
-//"<button class='button button--table' data-action = 'benchmark' data-band='"+band+"'>" + band + "</button>";
 
 function refreshAll(){
-	ribbon.registerActiveModes();
+	ribbon.registerActiveBandsAndModes();
 	mode = ribbon.getWatchedMode();
-    myCall1 = myCall.split(",")[0].trim();
-	console.log("Refresh with mode = "+ mode+ " myCall1 = "+myCall1);
-}
-
-function writeGrid(){
-	let html = "";
-	html+="<div style = 'display:grid; grid-template-columns:max-content 1fr 1fr;'>";
-	html+="<div></div><div class='directionHeading'>Receiving</div><div class='directionHeading'>Received By</div>"
-	for (const band of activeBands) {
-		html += "<div class = 'bandHeading' id="+band+"_heading>"+band+"</div><div class='receive' id="+band+"_rx><canvas id="+band+'_rx_canvas'+"></canvas></div><div class='transmit' id="+band+"_tx><canvas id="+band+'_tx_canvas'+"></canvas></div>";	
-	}
-	document.getElementById("mainView").innerHTML = html;
+	bands = Array.from(ribbon.getActiveBands());
+//	console.log("Refresh with mode = "+ mode+ " myCall1 = "+myCall1 + " bands = "+bands);
+	refreshBands();
 }
 
 function analyseData(data){
 	// data is subset of connectivity map e.g. data = connsData[band][mode].Tx
     let myCall1 = myCall.split(",")[0].trim();
-	homeCalls = new Set();
-	ocs_myCall1 = new Set();
-	ocs_Leader = new Set();
-	ocs_All = new Set();
+	let homeCalls = new Set();
+	let ocs_myCall1 = new Set();
+	let ocs_Leader = new Set();
+	let ocs_All = new Set();
+	let leaderCall = null;
 	
 	for (const hc in data) {
 		homeCalls.add(hc);
@@ -95,58 +85,62 @@ function analyseData(data){
 			ocs_myCall1 = ocs;
 		}
     }
-	homeCalls = Array.from(homeCalls);
-	ocs_myCall1 = Array.from(ocs_myCall1);
-	ocs_Leader = Array.from(ocs_Leader);
-	ocs_All = Array.from(ocs_All);
+	let dataset = {
+	homeCalls:Array.from(homeCalls),
+	myCall1:Array.from(ocs_myCall1),
+	Leader:Array.from(ocs_Leader),
+	All:Array.from(ocs_All),
+	leaderCall:leaderCall
+	}
+	return dataset;
 }
 
-function hideBand(band){
-	document.getElementById(band+'_heading').classList.add("hidden");
-	document.getElementById(band+'_rx').classList.add("hidden");
-	document.getElementById(band+'_tx').classList.add("hidden");
-}
-function unhideBand(band){
-	document.getElementById(band+'_heading').classList.remove("hidden");
-	document.getElementById(band+'_rx').classList.remove("hidden");
-	document.getElementById(band+'_tx').classList.remove("hidden");
-}
-function refreshBand(){
-	if(activeBands.length==0) return;
-	band_idx = (band_idx + 1) % activeBands.length; 
-	let band = activeBands[band_idx];
-	if(!liveConnsData[band]){
-		hideBand(band);
-		return;
+function refreshBands(){
+	for (let i =0;i<15;i++){
+		let div = 'div_'+i;
+		document.getElementById(div).classList.add("hidden");	
+		let canvas = 'canvas_'+i;
+		if(charts[canvas]){
+			charts[canvas].destroy()
+		}
 	}
-	if(!liveConnsData[band][mode]){
-		hideBand(band);
-		return;
+	for (const bandIdx in bands){
+		let div = 'div_'+bandIdx;
+		document.getElementById(div).classList.remove("hidden");			
+		let canvas = 'canvas_'+bandIdx;
+		let band = bands[bandIdx];
+		refreshBand(canvas,band);
 	}
-	unhideBand(band);
-	doChart(band+'_rx_canvas', liveConnsData[band][mode]?.Rx);
-	doChart(band+'_tx_canvas', liveConnsData[band][mode]?.Tx);	
 }
-		
-function doChart(canvas, connsData){
-	analyseData(connsData);
+
+function refreshBand(canvas, band){
+
+	let rx_data =analyseData(liveConnsData[band]?.[mode]?.['Rx']);
+	let tx_data =analyseData(liveConnsData[band]?.[mode]?.['Tx']);
+	
 	const data = {
-	  datasets: [	{label: myCall1 + ": "+ocs_myCall1.length+" spots", data: ocs_myCall1, backgroundColor: 'rgba(255, 99, 132, 1)', pointRadius:4},
-					{label: leaderCall + ": "+ocs_Leader.length+" spots", data: ocs_Leader, backgroundColor: 'rgba(54, 162, 235, 0.7)', pointRadius:6},
-					{label: homeCalls.size +" Home Calls: "+ocs_All.length+" spots", data: ocs_All, backgroundColor: 'rgba(200, 162, 235, 0.7)', pointRadius:10}],
+	  datasets: [	
+				{	label:'All', 				data: rx_data.All, 		backgroundColor: colors.leader_rx, 	pointRadius:9	},
+				{	label:'All', 				data: tx_data.All, 		backgroundColor: colors.all_tx, 	pointRadius:9	},
+				{	label:rx_data.leaderCall, 	data: rx_data.Leader, 	backgroundColor: colors.leader_rx, 	pointRadius:5	},
+				{	label:tx_data.leaderCall, 	data: tx_data.Leader,	backgroundColor: colors.leader_tx, 	pointRadius:5	},
+				{	label:myCall1, 				data: rx_data.myCall1, 	backgroundColor: colors.myCall_rx, 	pointRadius:3	},
+				{	label:myCall1, 				data: tx_data.myCall1, 	backgroundColor: colors.myCall_tx, 	pointRadius:3	}
+				],
 	};
 	if(charts[canvas]){
 		charts[canvas].destroy()
 	}
 	charts[canvas] = new Chart(
-	  document.getElementById(canvas),
-		{type: 'scatter',data: data, options: {
-			    animation: false, 
-				plugins: {legend: {display:false, position:'top', labels:{boxWidth:10, padding:10}}},
-				    scales: {
-						x: {title: {display:false, text: 'Longitude'}, type: 'linear',position: 'bottom', max:180, min:-180},
-						y: {title: {display:false, text: 'Lattitude'}, type: 'linear',position: 'left', max:80, min:-80}
-					}
+			document.getElementById(canvas),
+			{type: 'scatter',data: data, options: {
+				animation: false, 
+				plugins: {	legend: {display:false},             
+							title: {display: true, align:'start', text: " "+band}},
+				scales: {
+					x: {display:false, title: {display:false, text: 'Longitude'}, type: 'linear',position: 'bottom' , max:180, min:-180},
+					y: {display:false, title: {display:false, text: 'Lattitude'}, type: 'linear',position: 'left', max:80, min: -80}
+				}
 			}
 		}
 	);	
